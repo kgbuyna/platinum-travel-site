de754284f9dfe808d04c25d45832fbc1aebc51a1
Kgbuyna <kgbuyna@gmail.com>
2021-05-08 11:31:34 +0800

    hamburger menu duusav

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -2,6 +2,8 @@ class MobileMenu {
   constructor() {
     this.menuIcon = document.querySelector(".site-header__menu-icon");
     this.menuContent = document.querySelector(".site-header__menu-content");
+    this.siteHeader = document.querySelector(".site-header");
+
     this.events();
   }
 
@@ -11,7 +13,8 @@ class MobileMenu {
 
   toggleTheMenu() {
     this.menuContent.classList.toggle("site-header__menu-content--is-visible");
-    console.log("Huuray clicked");
+    this.siteHeader.classList.toggle("site-header--is-expanded");
+    this.menuIcon.classList.toggle("site-header__menu-icon--close-x");
   }
 }
 
