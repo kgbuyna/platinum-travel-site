8b655d9dd1ad3337848f79082ac6d8ebf57f6c7d
Kgbuyna <kgbuyna@gmail.com>
2021-05-10 14:02:59 +0800

    sticky header duuslaa

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,10 @@
 import "../styles/styles.css";
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
+import StickyHeader from "./modules/StickyHeader";
 
-new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
+let stickyHeader = new StickyHeader();
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)();
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 50);
 
 let mobileMenu = new MobileMenu();
