348a172b27bb2e5b01d5c55818a538d5f4ee48a6
Kgbuyna <kgbuyna@gmail.com>
2021-05-13 14:23:31 +0800

    build process n tohirgoog hiij duusav

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
+const currentTask = process.env.npm_lifecycle_event;
 const path = require("path");
-
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const fse = require("fs-extra");
 const postCSSPlugins = [
   require("postcss-import"),
   require("postcss-mixins"),
@@ -9,28 +13,42 @@ const postCSSPlugins = [
   require("autoprefixer"),
 ];
 
-module.exports = {
-  entry: "./app/assets/scripts/App.js",
-  output: {
-    filename: "bundled.js",
-    path: path.resolve(__dirname, "app"),
-  },
+class RunAfterCompile {
+  apply(compiler) {
+    compiler.hooks.done.tap("copy images", function () {
+      fse.copySync("./app/assets/images", "./docs/assets/images");
+    });
+  }
+}
 
-  mode: "development",
-  watch: true,
+let cssConfig = {
+  test: /\.css$/i,
+  use: [
+    "css-loader",
+    {
+      loader: "postcss-loader",
+      options: { postcssOptions: { plugins: postCSSPlugins } },
+    },
+  ],
+};
+
+let pages = fse
+  .readdirSync("./app")
+  .filter(function (file) {
+    return file.endsWith(".html");
+  })
+  .map(function (page) {
+    return new HtmlWebpackPlugin({
+      filename: page,
+      template: `./app/${page}`,
+    });
+  });
+let config = {
+  entry: "./app/assets/scripts/App.js",
+  plugins: pages,
   module: {
     rules: [
-      {
-        test: /\.css$/i,
-        use: [
-          "style-loader",
-          "css-loader",
-          {
-            loader: "postcss-loader",
-            options: { postcssOptions: { plugins: postCSSPlugins } },
-          },
-        ],
-      },
+      cssConfig,
       {
         test: /\.(png|svg|jpg|jpeg|gif|)$/i,
         type: "asset/resource",
@@ -38,3 +56,44 @@ module.exports = {
     ],
   },
 };
+
+if (currentTask == "dev") {
+  cssConfig.use.unshift("style-loader")(
+    (config.output = {
+      filename: "bundled.js",
+      path: path.resolve(__dirname, "app"),
+    })
+  ),
+    (config.mode = "development");
+}
+
+if (currentTask == "build") {
+  config.module.rules.push({
+    test: /\.js$/,
+    exclude: /(node_modules)/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: ["@babel/preset-env"],
+      },
+    },
+  });
+  cssConfig.use.unshift(MiniCssExtractPlugin.loader);
+  postCSSPlugins.push(require("cssnano"));
+  (config.output = {
+    filename: "[name].[chunkhash].js",
+    chunkFilename: "[name].[chunkhash].js",
+    path: path.resolve(__dirname, "docs"),
+  }),
+    (config.mode = "production");
+  config.optimization = {
+    splitChunks: { chunks: "all" },
+  };
+  config.plugins.push(
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({ filename: "styles.[chunkhash].css" }),
+    new RunAfterCompile()
+  );
+}
+
+module.exports = config;
